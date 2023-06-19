00a820146ed39ce789277ab50afa30c3d2dc2f65
rockethelll <rockethell@hotmail.com>
2023-06-19 18:48:27 +0200

    add rules to rubocop

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,18 +7,18 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private
 
   def respond_with(resource, _opts = {})
-    if request.method == "POST" && resource.persisted?
+    if request.method == 'POST' && resource.persisted?
       render json: {
-        status: {code: 200, message: 'Signed up sucessfully.'},
+        status: { code: 200, message: 'Signed up sucessfully.' },
         data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
       }, status: :ok
-    elsif request.method == "DELETE"
+    elsif request.method == 'DELETE'
       render json: {
-        status: { code: 200, message: 'Account deleted successfully.'}
+        status: { code: 200, message: 'Account deleted successfully.' }
       }, status: :ok
     else
       render json: {
-        status: {code: 422, message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}"}
+        status: { code: 422, message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}" }
       }, status: :unprocessable_entity
     end
   end
