517f5823fefa0dcda6845650b016a1246182a9d0
rockethelll <rockethell@hotmail.com>
2023-06-20 17:09:02 +0200

    add test for tmdb movies controller

--- spec/requests/movies_spec.rb
+++ spec/requests/movies_spec.rb
@@ -1,7 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'Movies' do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET /' do
+    before do
+      get '/'
+    end
+
+    it 'responds with ok status' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'contains a results key' do
+      expect(response.parsed_body).to have_key('results')
+    end
+
+    it 'contains at least one movie' do
+      expect(response.parsed_body['results']).not_to be_empty
+    end
   end
 end
