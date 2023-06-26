267e4ea36f510c982ca956654fac1cf656d11b2e
Theo <theo.gianella@proton.me>
2023-06-26 21:22:13 +0200

    Implemented logic for adding and removing movies to watchlists

--- app/controllers/watchlists_controller.rb
+++ app/controllers/watchlists_controller.rb
@@ -1,11 +1,25 @@
 class WatchlistsController < ApplicationController
-  def show
-    @watchlist = current_user.watchlist
+  before_action get_watchlist
 
+  def show
     render json: @watchlist.movies
   end
 
-  def include_movie; end
+  def include_movie
+    movie_id = params[:movie_id]
 
-  def remove_movie; end
+    @watchlist.movies << Movie.find(movie_id)  
+  end
+
+  def remove_movie
+    movie_id = params[:movie_id]
+
+    @watchlist.movies.delete_if { |movie| movie.id == movie_id }
+  end
+
+  private
+
+  def get_watchlist
+    @watchlist = current_user.watchlist
+  end
 end
