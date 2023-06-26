b53bae67e76c6d542eeddd2a828a67c5805f0a00
Theo <theo.gianella@proton.me>
2023-06-26 20:00:10 +0200

    Testing watchlists#get

--- spec/requests/watchlists_spec.rb
+++ spec/requests/watchlists_spec.rb
@@ -1,24 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe 'Watchlists' do
-  describe 'GET /show' do
+  describe 'GET /watchlist' do
+    before do
+      user = User.create(email: 'test@test.com', password: 'foobar')
+      movie = Movie.create(id: 1, title: 'test')
+      Watchlist.create(user: user, movies: [movie])
+      sign_in user
+      get '/watchlist'
+    end
+
     it 'returns http success' do
-      get '/watchlists/show'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'returns a movie if it is included in the watchlist' do
+      expect(response.parsed_body.first['id']).to eq 1
     end
   end
 
-  describe 'GET /include_movie' do
-    it 'returns http success' do
-      get '/watchlists/include_movie'
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'GET /remove_movie' do
-    it 'returns http success' do
-      get '/watchlists/remove_movie'
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe 'GET /include_movie' do
+  #   it 'returns http success' do
+  #     get '/watchlists/include_movie'
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
+  #
+  # describe 'GET /remove_movie' do
+  #   it 'returns http success' do
+  #     get '/watchlists/remove_movie'
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 end
