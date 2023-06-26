e4129037bc1009cead34e10e580e84699dc0498a
Theo <theo.gianella@proton.me>
2023-06-26 11:44:20 +0200

    Testing model

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Movie do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'creates a record in the database' do
+    movie = Movie.new(id: 1, title: 'test')
+    expect(movie.save).to be true
+  end
+
+  it "doesn't create a record in the database if no ID is specified" do
+    movie = Movie.new(title: 'test')
+    expect { movie.save }.to raise_error ActiveRecord::NotNullViolation
+  end
+
+  it "doesn't create a record in the database if no title is specified" do
+    movie = Movie.new(id: 1)
+    expect { movie.save }.to raise_error ActiveRecord::NotNullViolation
+  end
+
+  it "doesn't create a record in the database if ID is not unique" do
+    movie1 = Movie.create(id: 1, title: 'test')
+    movie2 = Movie.new(id: 1, title: 'test')
+    expect { movie2.save }.to raise_error ActiveRecord::RecordNotUnique
+  end
 end
