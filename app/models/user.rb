65009497622205ea82037edbc2d5e2d203591c26
Theo <theo.gianella@proton.me>
2023-06-26 17:19:48 +0200

    Creating table watchlist and one-to-one relationship with user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
+
+  has_one :watchlist
 end
---
+++ spec/models/watchlist_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Watchlist do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
