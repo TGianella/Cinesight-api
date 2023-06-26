6587863d0ffd02b9176d7a0fae1552d992eadac3
Theo <theo.gianella@proton.me>
2023-06-26 21:53:35 +0200

    Improved logic for #include and #remove movies, now sends back appropriate status codes to client

--- app/controllers/watchlists_controller.rb
+++ app/controllers/watchlists_controller.rb
@@ -1,25 +1,38 @@
 class WatchlistsController < ApplicationController
   before_action :get_watchlist
+  before_action only: %i[include_movie remove_movie] do
+    @movie = get_movie(params[:movie_id])
+  end
 
   def show
     render json: @watchlist.movies
   end
 
   def include_movie
-    movie_id = params[:movie_id]
-
-    @watchlist.movies << Movie.find(movie_id)  
+    if @watchlist.movies.include? movie
+      head :conflict
+    else
+      @watchlist.movies << Movie.find(movie_id)
+      head :ok
+    end
   end
 
   def remove_movie
-    movie_id = params[:movie_id]
-
-    @watchlist.movies.delete_if { |movie| movie.id == movie_id }
+    if @watchlist.movies.include? movie
+      @watchlist.movies.delete_if { |movie| movie.id == movie_id }
+      head :ok
+    else
+      head :not_found
+    end
   end
 
   private
 
   def get_watchlist
     @watchlist = current_user.watchlist
+  end
+
+  def get_movie(movie_id)
+    @movie = Movie.find(movie_id)
   end
 end
