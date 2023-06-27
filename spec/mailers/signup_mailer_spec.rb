0b7196c55ef3406083848bc53f1d3491149790dc
rockethelll <rockethell@hotmail.com>
2023-06-27 11:17:46 +0200

    add tests for signupMailer

--- spec/mailers/signup_mailer_spec.rb
+++ spec/mailers/signup_mailer_spec.rb
@@ -1,5 +1,23 @@
-require 'rails_helper'
+require_relative '../rails_helper'
 
 RSpec.describe SignupMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'signup mailer' do
+    it 'sends an email at signup' do
+      User.create(email: 'test@mail.fr', password: 'foobar')
+
+      expect(described_class.deliveries.count).to eq(1)
+    end
+
+    it 'sends an email to the user' do
+      User.create(email: 'test@mail.fr', password: 'foobar')
+
+      expect(described_class.deliveries.first.to).to eq(['test@mail.fr'])
+    end
+
+    it 'sends an email from the correct address' do
+      User.create(email: 'test@mail.fr', password: 'foobar')
+
+      expect(described_class.deliveries.first.from).to eq(['saeros@yopmail.com'])
+    end
+  end
 end
