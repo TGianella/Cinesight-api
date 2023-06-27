cca7bb21ecd98824efb07d2ae21ad40ff11c6366
Theo <theo.gianella@proton.me>
2023-06-27 17:24:15 +0200

    Abstracted logic of recording movie from TMDB to class method Movie#create_from_tmdb

--- app/controllers/concerns/import_movie.rb
+++ app/controllers/concerns/import_movie.rb
@@ -20,25 +20,11 @@ module ImportMovie
       genres = get_genres(details_body[:genres])
       director = get_director(movie_id)
 
-      create_movie(details_body, director, genres)
+      Movie.create_from_tmdb(details_body, director, genres)
     end
 
     def parse_response(response)
       JSON.parse(response.body, { symbolize_names: true })
-    end
-
-    def create_movie(details_body, director, genres)
-      Movie.create(id: details_body[:id],
-                   title: details_body[:title],
-                   vote_average: details_body[:vote_average],
-                   vote_count: details_body[:vote_count],
-                   poster_path: details_body[:poster_path],
-                   original_title: details_body[:original_title],
-                   overview: details_body[:overview],
-                   release_date: details_body[:release_date],
-                   tagline: details_body[:tagline],
-                   director: director[:name],
-                   genres: genres)
     end
 
     def update_director(movie_id)
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,18 @@ class Movie < ApplicationRecord
   self.primary_key = :id
   has_and_belongs_to_many :watchlists
   has_and_belongs_to_many :genres
+
+  def self.create_from_tmdb(details_body, director, genres)
+    Movie.create(id: details_body[:id],
+                 title: details_body[:title],
+                 vote_average: details_body[:vote_average],
+                 vote_count: details_body[:vote_count],
+                 poster_path: details_body[:poster_path],
+                 original_title: details_body[:original_title],
+                 overview: details_body[:overview],
+                 release_date: details_body[:release_date],
+                 tagline: details_body[:tagline],
+                 director: director[:name],
+                 genres: genres)
+  end
 end
