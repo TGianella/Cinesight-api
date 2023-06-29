01fbf68244f7ac3f31cb88d87ba2f232eaba0cf0
Theo <theo.gianella@proton.me>
2023-06-29 16:21:22 +0200

    Added endpoints for similar movies and search by genre

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -29,9 +29,25 @@ class MoviesController < ApplicationController
     render json: response
   end
 
+  def similar
+    movie_id = params[:id]
+    url = "https://api.themoviedb.org/3/movie/#{movie_id}/similar"
+    response = query_external_db(url)
+
+    render json: response
+  end
+
   def search
     query = params[:query]
     url = "https://api.themoviedb.org/3/search/movie?query=#{query}&include_adult=false&language=fr-FR&page=1"
+    response = query_external_db(url)
+
+    render json: response
+  end
+
+  def discover
+    genre_id = params[:genre_id]
+    url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=fr-FR&page=1&sort_by=popularity.desc&with_genres=#{genre_id}"
     response = query_external_db(url)
 
     render json: response
--- spec/requests/movies_spec.rb
+++ spec/requests/movies_spec.rb
@@ -73,6 +73,42 @@ RSpec.describe 'Movies' do
     end
   end
 
+  describe 'GET /movie/:id/similar' do
+    before do
+      get '/movie/2/similar'
+    end
+
+    it 'responds with ok status' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'contains a results key' do
+      expect(response.parsed_body).to have_key('results')
+    end
+
+    it 'contains at least one movie' do
+      expect(response.parsed_body['results']).not_to be_empty
+    end
+  end
+
+  describe 'GET /movies/discover' do
+    before do
+      get '/movies/discover?genre_id=80'
+    end
+
+    it 'responds with ok status' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'contains a results key' do
+      expect(response.parsed_body).to have_key('results')
+    end
+
+    it 'contains at least one movie' do
+      expect(response.parsed_body['results']).not_to be_empty
+    end
+  end
+
   describe 'GET /search' do
     before do
       get '/movies/search?query=test'
